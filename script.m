eb42b1ae6f99e339c735d85c1f40733f3fe12610
ericmassol <massoleric07@gmail.com>
2024-11-25 18:22:17 +0100

    propositions.... Toujours pas fonctionnel

--- script.m
+++ script.m
@@ -1,11 +1,11 @@
-clear all
-close all
+clear all;
+close all;
 
 % Chargement des données
 load carte.dat
 load mesure_accelero
 
-%LÉGENDES
+% LÉGENDES
 % Pour carte.dat : numéro d'amer = numéro de colonne et X = carte(1,:), Y = carte(2,:) Z = carte(3,:)
 % Pour mesure_accelero : temps = mesure_accelero(:,1), aX = mesure_accelero(:,2), aY = mesure_accelero(:,3), aZ = mesure_accelero(:,4)
 % Pour image : image(1,:) = numéro d'amer, U = image(2,:), V = image(3,:)
@@ -13,29 +13,35 @@ load mesure_accelero
 % --- AFFICHAGES INITIAUX ---
 
 % Visualisation des amers
-stem3(carte(1, :), carte(2, :), carte(3, :))
+stem3(carte(1, :), carte(2, :), carte(3, :));
 axis([0 6000 -1000 1000 -100 1000]);
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
+title('Amers');
 
-%Visualisation des mesures d'accélération
-figure
-plot(mesure_accelero(:, 1), mesure_accelero(:, 2), 'r', mesure_accelero(:, 1), mesure_accelero(:, 3), 'b', mesure_accelero(:, 1), mesure_accelero(:, 4), 'g')
+% Visualisation des mesures d'accélération
+figure;
+plot(mesure_accelero(:, 1), mesure_accelero(:, 2), 'r', mesure_accelero(:, 1), mesure_accelero(:, 3), 'b', mesure_accelero(:, 1), mesure_accelero(:, 4), 'g');
 xlabel('temps (s)');
 ylabel('acceleration (m/s²)');
 title('Mesures d''accélération');
 legend('axe X', 'axe Y', 'axe Z');
 
-%Choix d'une image spécifique pour le plot
+% --- PARAMÈTRES GÉNÉRAUX ---
+pas = 10 ^ (-2);
+g = 1.622;
+f = 512; % Distance focale
+df = 512;
+
+% --- INITIALISATION ---
 k_plot_image = 50;
 
 for k = 0:5
     % Chargement de l'image
-    [filename, err] = sprintf('images/image%3.3d', k);
+    filename = sprintf('images/image%3.3d', k);
     image = load(filename);
 
-    % Affichage de l'image
     if k == k_plot_image
         figure;
         plot(image(2, :), image(3, :), '.');
@@ -45,29 +51,23 @@ for k = 0:5
         title(['Projection de l''image ', num2str(k)]);
     end
 
-    % --- INITIALISATION ---
-
-    % initialisez la moyenne et la covariance de l'état en utilisant
-    % des couples d'amers et une idée sur la vitesse initiale et les
-    % biais des accéléromètres
-    if (k == 0)
+    if k == 0
         Xpos = [];
         Ypos = [];
         Zpos = [];
-        df = 512;
         n = 1;
 
+        % Calcul initial des positions
         for j = 1:size(image, 2)
+            XA = carte(1, image(1, j));
+            YA = carte(2, image(1, j));
+            ZA = carte(3, image(1, j));
+            UA = image(2, j);
+            VA = image(3, j);
 
-            for i = 1:size(image, 1)
+            for i = 1:size(image, 2)
 
                 if i ~= j
-                    XA = carte(1, image(1, j));
-                    YA = carte(2, image(1, j));
-                    ZA = carte(3, image(1, j));
-                    UA = image(2, j);
-                    VA = image(3, j);
-
                     XB = carte(1, image(1, i));
                     YB = carte(2, image(1, i));
                     ZB = carte(3, image(1, i));
@@ -75,13 +75,12 @@ for k = 0:5
                     VB = image(3, i);
 
                     YE = 1 / (UA / VA - UB / VB) * (XB - XA + UA / VA * YA - UB / VB * YB);
-                    XE = XA + (YE -YA) * UA / VA;
+                    XE = XA + (YE - YA) * UA / VA;
                     ZE = ZA + df / UA * (XA - XE);
 
                     Xpos(n) = XE;
                     Ypos(n) = YE;
                     Zpos(n) = ZE;
-
                     n = n + 1;
                 end
 
@@ -89,126 +88,75 @@ for k = 0:5
 
         end
 
-        %Calcul des moyennes
+        % Calcul des moyennes et covariances
         Xavg = mean(Xpos);
         Yavg = mean(Ypos);
         Zavg = mean(Zpos);
-
-        %Calcul des variances
-        data = [Xpos(:), Ypos(:), Zpos(:)];
-
-        % Calculer la matrice de covariance
-        covMatrix = cov(data);
+        covMatrix = cov([Xpos(:), Ypos(:), Zpos(:)]);
         covMatrix = diag(diag(covMatrix));
 
-        % Commentaire
-        %On a un écart type pour la position en X de 50m environ, la précision
-        %du capteur est donc de 50m ce qui est raisonnable étant donné que l'on
-        %se situe en moyenne à 1000m des points que l'on observe
-        %(mean(Z)=999.98m)
-
-        %Construction de la moyenne et de la covariance pour la vitesse.
-        Vavg = [100, 0, 0]; %j'ai modifié la taille de Vavg (Eric)
+        % Initialisation des vitesses et biais
+        Vavg = [100; 0; 0];
         covV = diag([2 ^ 2, 2 ^ 2, 2 ^ 2]);
-
         biaisavg = [0; 0; 0];
         covbiais = diag([0.2 ^ 2, 0.2 ^ 2, 0.2 ^ 2]);
     end
 
-    % --- PARAMÈTRES DU FILTRE DE KALMAN ---
-
-    pas = 10 ^ (-2);
-    g = 1.622;
-    X = [[Xavg; Yavg; Zavg]; Vavg'; biaisavg]; % État initial
+    % Initialisation des matrices du filtre
+    X = [[Xavg; Yavg; Zavg]; Vavg; biaisavg];
     Pk = blkdiag(covMatrix, covV, covbiais);
 
-    traj = zeros(9, 99); % 9 états sur 99 itérations
+    traj = zeros(9, 99);
 
-    Y = X; % SUR ?
+    for l = 1:99
+        % Matrices A et B
+        A = [eye(3), eye(3) * pas, zeros(3); zeros(3), eye(3), -eye(3) * pas; zeros(3), zeros(3), eye(3)];
+        B = [zeros(3); eye(3) * pas; zeros(3)];
+        COVU = blkdiag(covMatrix, covV, covbiais);
 
-    if (k ~= 100)
+        % Prédiction
+        Z = A * X + B * [mesure_accelero(100 * k + l, 2); mesure_accelero(100 * k + l, 3); mesure_accelero(100 * k + l, 4) - g];
+        COVY = A * Pk * A' + COVU;
 
-        for l = 1:1:99
-            %L'accéléromètre va 100 a une fréquence 100 fois plus rapide
-            %que le système, ce qui explique pourquoi la double boucle, on
-            %doit donc garder la dernière valeur pour mettre à jour l'image.
+        % Recalage
+        S = reshape(image(2:end, :), [], 1);
+        n = size(image, 2);
+        Hz = zeros(2 * n, 1);
+        dHz = zeros(9, 2 * n);
 
-            % utilisez mesure_accelero(100*k+l,2), mesure_accelero(100*k+l,3) et
-            % mesure_accelero(100*k+l,4) pour prédire moyenne et
-            % covariance de l'état au temps k+0.01*(l+1)
+        for i = 1:n
+            Hz(2 * i - 1) = f * (Xpos(i) - Z(1)) / (Z(3) - Zpos(i));
+            Hz(2 * i) = f * (Ypos(i) - Z(2)) / (Z(3) - Zpos(i));
 
-            % Prédiction
-            A = [eye(3), eye(3) * pas, zeros(3);
-                 zeros(3), eye(3), -eye(3) * pas;
-                 zeros(3), zeros(3), eye(3)];
-            B = [zeros(3); eye(3) * pas; zeros(3)];
-            COVU = blkdiag(covMatrix, covV, covbiais);
+            dHz(1, 2 * i - 1) = -f / (Z(3) - Zpos(i));
+            dHz(2, 2 * i - 1) = 0;
+            dHz(3, 2 * i - 1) = f * (Xpos(i) - Z(1)) / (Z(3) - Zpos(i)) ^ 2;
 
-            %Construction de l'estimation à priori
-            Z = A * Y + B * [mesure_accelero(100 * k + l, 2);
-                             mesure_accelero(100 * k + l, 3);
-                             mesure_accelero(100 * k + l, 4) - g];
-            COVY = A * Pk * A' + COVU;
+            dHz(1, 2 * i) = 0;
+            dHz(2, 2 * i) = -f / (Z(3) - Zpos(i));
+            dHz(3, 2 * i) = f * (Ypos(i) - Z(2)) / (Z(3) - Zpos(i)) ^ 2;
+        end
 
-            % Recalage
-            S = reshape(image(2:end, :), [], 1);
-            f = 512; % Distance focale
-            n = size(image, 2); % Nombre d'amers
+        dHz = dHz';
 
-            % Construction de h(z) et de sa jacobienne
-            Hz = zeros(2 * n, 1);
-            dHz = zeros(9, 2 * n);
+        % Mise à jour
+        COVV = eye(size(Hz, 1));
+        K = COVY * dHz' * (dHz * COVY * dHz' + COVV) ^ (-1);
+        X = Z + K * (S - Hz);
+        Pk = (eye(9) - K * dHz) * COVY;
 
-            %Calcul de h(z)
-            for i = 1:n
-                % J'ai corrigé puisque c'était H(x) et non h(z) qu'on avait mis en séance... (Eric)
-                Hz(2 * i - 1) = f * (Xpos(i) - Z(1)) / (Z(3) - Zpos(i));
-                Hz(2 * i) = f * (Ypos(i) - Z(2)) / (Z(3) - Zpos(i));
+        traj(:, l) = X;
 
-                % Dérivées par rapport aux états (position)
-                dHz(1, 2 * i - 1) = -f / (Z(3) - Zpos(i));
-                dHz(2, 2 * i - 1) = 0;
-                dHz(3, 2 * i - 1) = f * (Xpos(i) - Z(1)) / (Z(3) - Zpos(i)) ^ 2;
-
-                dHz(1, 2 * i) = 0;
-                dHz(2, 2 * i) = -f / (Z(3) - Zpos(i));
-                dHz(3, 2 * i) = f * (Ypos(i) - Z(2)) / (Z(3) - Zpos(i)) ^ 2;
-
-                % Les autres états (vitesses et biais) ne contribuent pas aux projections
-                dHz(4:9, 2 * i - 1) = 0;
-                dHz(4:9, 2 * i) = 0;
-            end
-
-            % On transpose dHz pour avoir la bonne dimension
-            dHz = dHz';
-
-            % Correction %
-            %Mise à jour de l'estimation à posteriori
-            COVV = eye(size(Hz, 1));
-            K = COVY * dHz' * (dHz * COVY * dHz' + COVV) ^ (-1);
-            Y = Z + K * (S - Hz);
-            Pk_1 = (eye(9) - K * dHz) * COVY;
-
-            if mod(l, 20) == 0 % Enregistrer toutes les 50 itérations
-                figure(1);
-                plot(S, 'o-', 'DisplayName', 'S (Observations)');
-                hold on;
-                plot(Hz, 'x-', 'DisplayName', 'Hz (Projections estimées)');
-                legend;
-                xlabel('Index des observations');
-                ylabel('Valeur');
-                title(['Comparaison entre S et Hz à l''itération ', num2str(l)]);
-                hold off;
-                saveas(gcf, ['comparaison_S_Hz_iter_' num2str(l) '.png']); % Enregistrer l'image
-            end
-
-            % Calcul de l'erreur
-            erreur = abs(S - Hz);
-            disp(['Erreur max à l''itération ', num2str(l), ': ', num2str(max(erreur))]);
-
-            % Stockage des trajectoires
-            traj(:, l) = Y;
-
+        if mod(l, 20) == 0
+            figure;
+            plot(S, 'o-', 'DisplayName', 'S (Observations)');
+            hold on;
+            plot(Hz, 'x-', 'DisplayName', 'Hz (Projections estimées)');
+            legend;
+            xlabel('Index des observations');
+            ylabel('Valeur');
+            title(['Comparaison entre S et Hz à l''itération ', num2str(l)]);
+            hold off;
         end
 
     end
@@ -216,7 +164,6 @@ for k = 0:5
 end
 
 % --- AFFICHAGE DES TRAJECTOIRES ---
-
 figure;
 subplot(3, 1, 1);
 plot(1:99, traj(1, :), '-r', 'DisplayName', 'Position X');
@@ -239,16 +186,12 @@ ylabel('Z (m)');
 title('Trajectoire - Position Z');
 legend;
 
-% --- AFFICHAGE DES BIAIS ESTIMÉS ---
-
-%On remarque que l'on a notamment un problème sur le biais de X, qui ne commence pas à 0.
-%Il faudrait donc revoir la manière dont on initialise les biais.
-
+% --- AFFICHAGE DES BIAIS ---
 figure;
-plot(1:99, traj(4, :), '-r', 'DisplayName', 'Biais X'); % Biais sur X
+plot(1:99, traj(7, :), '-r', 'DisplayName', 'Biais X');
 hold on;
-plot(1:99, traj(5, :), '-g', 'DisplayName', 'Biais Y'); % Biais sur Y
-plot(1:99, traj(6, :), '-b', 'DisplayName', 'Biais Z'); % Biais sur Z
+plot(1:99, traj(8, :), '-g', 'DisplayName', 'Biais Y');
+plot(1:99, traj(9, :), '-b', 'DisplayName', 'Biais Z');
 xlabel('Itérations');
 ylabel('Valeur des biais');
 title('Évolution des biais estimés');
